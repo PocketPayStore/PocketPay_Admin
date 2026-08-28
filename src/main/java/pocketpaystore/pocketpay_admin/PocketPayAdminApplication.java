3e18e8e551bd7c2b312049bddafd48dd04ba9ab9
ehddbs4521 <ehddbs452100@naver.com>
2026-08-28 19:56:34 +0900

    init project

---
+++ src/main/java/pocketpaystore/pocketpay_admin/PocketPayAdminApplication.java
@@ -0,0 +1,13 @@
+package pocketpaystore.pocketpay_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketPayAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketPayAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pocketpaystore/pocketpay_admin/PocketPayAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package pocketpaystore.pocketpay_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketPayAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
