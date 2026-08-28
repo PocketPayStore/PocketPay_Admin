04800f6ab5b7044048f0d6b4169a6f2dffe8d7dd
ehddbs4521 <ehddbs452100@naver.com>
2026-08-29 00:48:45 +0900

    feat: #2 verify sse delivery

--- src/test/java/pocketpaystore/pocketpay_admin/PocketPayAdminApplicationTests.java
+++ src/test/java/pocketpaystore/pocketpay_admin/PocketPayAdminApplicationTests.java
@@ -3,7 +3,12 @@ package pocketpaystore.pocketpay_admin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"payment-events.enabled=false",
+		"sse.timeout=30000",
+		"sse.heartbeat-interval=15000",
+		"sse.reconnect-time=3000"
+})
 class PocketPayAdminApplicationTests {
 
 	@Test
